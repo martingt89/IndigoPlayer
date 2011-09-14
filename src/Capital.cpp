0f53e70b343ece80db3c71f903c0b0f61cd5df48
Martingt89 <Martingt89@gmail.com>
2011-09-14 22:40:30 +0200

    work on i18n

--- src/Capital.cpp
+++ src/Capital.cpp
@@ -16,8 +16,15 @@
 #include "Files/Logger.h"
 #include "Files/FileUtilities.h"
 
+#include <libintl.h>
+#include <config.h>
 
 int main(int argc, char *argv[]){
+
+	bindtextdomain(GETTEXT_PACKAGE, DATADIR"/locale");
+	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+	textdomain(GETTEXT_PACKAGE);
+  
 	Glib::thread_init();
 	Gtk::Main kit(argc, argv);
 	FileUtilities fu;
